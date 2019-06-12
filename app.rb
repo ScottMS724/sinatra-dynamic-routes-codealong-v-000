9601c32d8f44310f7f9deda0c51d6a054abd8e0d
Scott Silverman <scottms724@gmail.com>
2019-06-12 16:23:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,5 +19,10 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end 
+  
+  get '/multiply/:number1/:number2' do 
+    @product = params[:number1]*params[:number2]
+    @product
+  end 
 
 end
